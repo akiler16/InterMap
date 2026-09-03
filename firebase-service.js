b37c3fee2adfd9d6b76bf4a346f4b5f3006daf89
Ивар Рыбалка <153619192+akiler16@users.noreply.github.com>
2026-09-03 10:57:15 +0300

    Add persistence methods to Firebase auth

--- firebase-service.js
+++ firebase-service.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { 
     getAuth, 
+    setPersistence,
+    browserLocalPersistence,
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword, 
     signOut, 
